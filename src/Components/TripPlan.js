bb588155b1ed3f48ea005e7b83762f0f8889f9ff
sanad.c.a <sanadc@netstratum.com>
2023-01-28 18:02:52 +0530

    added new plan selector

--- src/Components/TripPlan.js
+++ src/Components/TripPlan.js
@@ -1,57 +1,103 @@
 import classes from './TripPlan.module.css';
-import styles from './TripPlan.css';
 import Box from '@mui/material/Box';
 import Slider from '@mui/material/Slider';
 import { useState } from 'react';
-import { styled } from '@mui/material/styles';
+import TextField from '@mui/material/TextField';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
 
 const TripPlan = () => {
 
-  function valuetext(value) {
-    return `${value}°C`;
+  function valuetext(budget) {
+    return `${budget}`;
+  };
+  const [email, setEmail] = useState('');
+  const [place, setPlace] = useState('');
+  const [dateValue, setDateValue] = useState(null);
+  const [budget, setBudget] = useState([20, 90]);
+
+  const handleChangeEmail = (event) => {
+    setEmail(event.target.value);
   };
 
-  const [value, setValue] = useState([20, 90]);
+  const handleChangePlace = (event) => {
+    setPlace(event.target.value);
+  };
 
   const handleChange = (event, newValue) => {
-    setValue(newValue);
+    setBudget(newValue);
   };
 
+  const onSubmitHandler = (event)=>{
+    event.preventDefault();
+  //send mail with email place datevalue budget here 
+  }
+  const today = new Date();
     return(
      <form className={classes.form}>
         <div className={classes.div001}>
-          <label>Where you want to go</label>
-          <select>
-            <option>( select )</option>
-            <option>United Kingdom</option>
-            <option>United States Of America</option>
-            <option>Middl East</option>
-            <option>Singapore</option>
-          </select>
+          <label>Enter Your Mail</label>
+          <Box sx={{ minWidth: 120 }} >
+            <TextField id="email" label="Email"  value={email} variant="outlined" onChange={handleChangeEmail}/>
+        </Box> 
+        </div>
+        <div className={classes.div001}>
+          <label>Where you want to go?</label>
+          <Box sx={{ minWidth: 120 }}>
+           <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Destination</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={place}
+              label="Destination"
+              onChange={handleChangePlace}
+             >
+            <MenuItem value={0}></MenuItem>  
+            <MenuItem value={10}>UK</MenuItem>
+            <MenuItem value={20}>USA</MenuItem>
+            <MenuItem value={30}>Middle East</MenuItem>
+            <MenuItem value={40}>Singapore</MenuItem>
+            </Select>
+          </FormControl>
+          </Box>    
         </div>
         <div className={classes.div001}>
           <label>When you are planning</label>
-          <select>
-            <option>( select )</option>
-            <option>1</option>
-          </select>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+           <DatePicker
+           label="Date"
+           minDate={today}
+           value={dateValue}
+           onChange={(newValue) => {
+            setDateValue(newValue);
+           }}
+            renderInput={(params) => <TextField {...params} />}
+          />
+         </LocalizationProvider>
         </div>
         <div className={classes.div001}>
-          <label>Select Your Budget</label>
+          <label>Select Your Budget($)</label>
           <Box sx={{ width: '100%' }}>
         <Slider
              className="slider"
              min={0}
              max={10000}     
              getAriaLabel={() => 'Budget'}
-             value={value}
+             value={budget}
              onChange={handleChange}
              valueLabelDisplay="auto"
             getAriaValueText={valuetext}
             />
          </Box>
         </div>
-        <button>Plan Your Trip</button>
+        <button onClickCapture={onSubmitHandler}>Plan Your Trip</button>
      </form>
 
 
