dba120bc68b8f4f3e5eedddb5e03a450a30106f7
roufkp <roufkpkp@gmail.com>
2023-05-12 22:44:48 +0530

    new

--- src/Components/Admin.js
+++ src/Components/Admin.js
@@ -6,6 +6,7 @@ import UpdateBlog from '../Components/Blog/updateBlog';
 import { List, ListItem,Paper, ListItemText, Typography } from '@material-ui/core';
 import upload from "../Assets/upload.gif";
 import UpdateTestimonial from './Testimonial/UpdateTestimonial';
+import back from "../Assets/back.png";
 
 
 
@@ -19,13 +20,15 @@ const Admin = () => {
         <>
             <Navigation/>
             <div className={styles.main}>
-            <button className={styles.listItem} style={{marginTop:"30px",marginLeft:"0"}} onClick={() => window.history.back()}>Go Back</button>
+            <button className={styles.listItem} style={{marginTop:"10px",marginLeft:"0",position:"absolute",left:"20px",width:"60px",minWidth:"40px"}} onClick={() => window.history.back()}>
+              <img src={back} alt="" style={{width:"20px"}} />
+            </button>
             <div className={styles.dashboard}>
-                <div>
-                    <img src={upload} alt="test"  style={{maxWidth:"270px"}}/>
-                </div>
+                {/* <div>
+                    <img src={upload} alt="test"  style={{maxWidth:"0px"}}/>
+                </div> */}
       <List className={styles.listContainer}>
-        <ListItem className={styles.listItem}  onClick={() => handleItemClick(0)}>
+        <ListItem className={styles.listItem}  onClick={() => handleItemClick(0)} >
           <ListItemText primary="Blog" />
         </ListItem>
         <ListItem className={styles.listItem}  onClick={() => handleItemClick(1)}>
@@ -34,19 +37,33 @@ const Admin = () => {
       </List>
       <div className={styles.sectionContainer}>
         {activeSection === null ? (
-          <Typography variant="h5" className={styles.defaultSection} style={{marginTop:"50px"}}>
+          <Typography variant="h5" className={styles.defaultSection} style={{marginTop:"50px",fontSize:"14px"}}>
             Please press a button upload content.
           </Typography>
         ) : (
           <div className={styles.fullScreen}>
-            {activeSection === 0 ? (
+            {activeSection === 0 ? (<>
               <div className={styles.section}>
-                <UpdateBlog/>
+                <div className={styles.side}>
+                  test
+                </div>
+                <div className={styles.details}>
+
+                </div>                
               </div>
-            ) : (
+              <UpdateBlog />
+              </>
+            ) : (<>
               <div className={styles.section}>
-                <UpdateTestimonial/>
+                <div className={styles.side}>
+                  test
+                </div>
+                <div className={styles.details}>
+
+                </div>                
               </div>
+              <UpdateTestimonial/>
+              </>
             )}
           </div>
         )}
